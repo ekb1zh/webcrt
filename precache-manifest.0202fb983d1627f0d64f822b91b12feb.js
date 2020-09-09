6d09af7062727ea5878c0636c75594240ba38d37
ekb1zh <ekb1zh@gmail.com>
2020-09-09 22:21:21 +0300

    Updates

---
+++ precache-manifest.0202fb983d1627f0d64f822b91b12feb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be4ef0bb01da21682b79135f570cfa65",
+    "url": "/webcrt/index.html"
+  },
+  {
+    "revision": "447af759d1e0c7327e84",
+    "url": "/webcrt/static/css/main.1fc63ada.chunk.css"
+  },
+  {
+    "revision": "52a845d46074d0b3fff9",
+    "url": "/webcrt/static/js/2.28ce4dae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/webcrt/static/js/2.28ce4dae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "447af759d1e0c7327e84",
+    "url": "/webcrt/static/js/main.012c56d1.chunk.js"
+  },
+  {
+    "revision": "6d16346a67d31e9e659c",
+    "url": "/webcrt/static/js/runtime-main.5ed1aa5b.js"
+  }
+]);
